f161f184a0d7a93d89b5f98477fdc1a55f4a9678
01Hari01 <harikrishnathoka@gmail.com>
2025-01-19 19:29:56 +0530

    correcting middleware order

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -57,14 +57,16 @@ CORS_ALLOW_METHODS = [
     "OPTIONS",
 ]
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+    # Default
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'djangoProject.urls'
