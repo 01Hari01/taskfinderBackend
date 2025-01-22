05504bfb83f29a033f0d6dc303551d12169258f8
01Hari01 <harikrishnathoka@gmail.com>
2025-01-22 13:58:28 +0530

    removing confirm password

--- task_calendar_backend/serializers/serializers.py
+++ task_calendar_backend/serializers/serializers.py
@@ -5,10 +5,11 @@ from task_calendar_backend.models import Task
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    confirm_password= serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['username', 'password', 'confirm_password']
+        fields = ['username', 'password']
 
     def validate(self, data):
         username = data.get('username', '')
@@ -17,14 +18,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
         if len(username) > 10:
             raise serializers.ValidationError({"username": "Username must be at most 10 characters."})
-        if password != confirm_password:
-            raise serializers.ValidationError({"password": "Passwords must match."})
+        if data['password'] != self.initial_data.get('confirm_password'):  # Compare with confirm_password
+            raise serializers.ValidationError({'password': 'Passwords must match'})
 
-    def create(self,validated_data):
+    def create(self, validated_data):
         validated_data.pop("confirm_password")
         user = User.objects.create_user(
             username=validated_data["username"],
-            password= validated_data["password"]
+            password=validated_data["password"]
         )
         return user
 
